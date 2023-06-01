cfc3a8a3a558c268f39030321067eeae131444eb
jay <jay.wekeep@gmail.com>
2023-06-01 09:22:10 +0900

    테스트 환경 추가

---
+++ src/test/java/me/jay/fcp/BatchTestSupport.java
@@ -0,0 +1,50 @@
+package me.jay.fcp;
+
+import me.jay.fcp.message.MessageRepository;
+import me.jay.fcp.point.PointRepository;
+import me.jay.fcp.point.reservation.PointReservationRepository;
+import me.jay.fcp.point.wallet.PointWalletRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class BatchTestSupport {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    PointWalletRepository pointWalletRepository;
+    @Autowired
+    PointRepository pointRepository;
+    @Autowired
+    MessageRepository messageRepository;
+    @Autowired
+    PointReservationRepository pointReservationRepository;
+
+    protected JobExecution launchJob(Job job, JobParameters jobParameters) throws Exception {
+        JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+        jobLauncherTestUtils.setJob(job);
+        jobLauncherTestUtils.setJobLauncher(jobLauncher);
+        jobLauncherTestUtils.setJobRepository(jobRepository);
+        return jobLauncherTestUtils.launchJob(jobParameters);
+    }
+
+    @AfterEach
+    protected void deleteAll() {
+        pointWalletRepository.deleteAll();
+        pointRepository.deleteAll();
+        messageRepository.deleteAll();
+        pointReservationRepository.deleteAll();
+    }
+}
